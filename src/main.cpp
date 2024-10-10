9ef6a64f7ae2c9a14a7400337550f45841e1b0e5
Gereon V <72784429+GereonV@users.noreply.github.com>
2024-10-10 13:13:59 +0200

    Add clang-tidy

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <string_view>
 
 namespace {
     void print(auto && obj) { std::cout << static_cast<decltype(obj)>(obj) << '\n'; }
 }
 
-int main() { print("Hello, world!"); }
+int main()
+{
+    using namespace std::string_view_literals;
+    print("Hello, world!"sv);
+}
